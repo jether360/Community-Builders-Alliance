bbf83d101650acf31f06f69d5cfc82ed1ff8d3fd
jether balabagno <balabagnojether@gmail.com>
2022-09-14 11:18:11 +0800

    added footer in public route

--- src/app/navigation/public/publicRoute.ts
+++ src/app/navigation/public/publicRoute.ts
@@ -1,6 +1,6 @@
 import { RouteProps } from "react-router-dom";
 import Home from "../../../components/public/home/Home";
-import About from "../../../components/public/about/About";
+
 
 const publicRoutes: RouteProps[] = [
     {
