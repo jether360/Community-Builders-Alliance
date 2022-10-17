ab7689cbe7fb7ad612de7a6018022c2e616568e9
jether balabagno <balabagnojether@gmail.com>
2022-10-17 11:04:10 +0800

    added register component

--- src/app/navigation/public/publicRoute.ts
+++ src/app/navigation/public/publicRoute.ts
@@ -1,11 +1,15 @@
 import { RouteProps } from "react-router-dom";
 import Home from "../../../components/public/home/Home";
-
+import Register from "../../../components/public/register/Register";
 
 const publicRoutes: RouteProps[] = [
     {
         path:"/",
         component:Home
+    },
+    {
+        path:"/register",
+        component:Register
     },
 ]
 export default publicRoutes;
